d52ccc70683da99441ccb006cf8cde423ea65002
Stahelstrauch <estahelstrauchpulk@gmail.com>
2025-05-26 19:44:46 +0300

    Põhiülesanded koos lisaülesandega.

--- src/View.java
+++ src/View.java
@@ -20,6 +20,7 @@ public class View {
 
     /**
      * Tagastab kasutaja sisestuse
+     *
      * @return kasutaja sisestus
      */
     public int getMenuChoice() { //Küsitakse valitut menüüst
@@ -28,6 +29,7 @@ public class View {
 
     /**
      * Väljastab etteantud teate konsooli
+     *
      * @param message teade mida väljastada
      */
     public void showMessage(String message) { //Näitab teksti, et ei peaks kirjutama koguaeg sout-i
@@ -36,12 +38,21 @@ public class View {
 
     /**
      * Küsib kasutajalt numbrit
+     *Kontrollib, et sisestus on täisarv
      * @return kasutaja sisestatud number
      */
     public int askGuess() {
-        System.out.print("Sisesta number: ");
-        return Integer.parseInt(scanner.nextLine()); //Kasutaja sisestus üritatakse teha täisarvuks, kui õnnestub siis väljastab
-    }
+        while (true) {
+            System.out.print("Sisesta number: ");
+            //return Integer.parseInt(scanner.nextLine()); //Kasutaja sisestus üritatakse teha täisarvuks, kui õnnestub siis väljastab
+            String input = scanner.nextLine();
+            try { //Kontrolliblokk. Kui täisarv siis tagastab kui miskit muu (tekst, murdarv jms) siis ütleb et ei sobi küsib uuesti.
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Sinu sisestatu ei sobi. Palun kirjuta täisarv vahemikus 1-100.");
+            }
+        }
+}
 
     /**
      * Küsib kasutajalt nime mängu lõpus
